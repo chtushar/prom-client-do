21c9b8c120b8ee14522cb2bf0b18f81e82d19172
Tushar Choudhari <choudharit23@gmail.com>
2024-10-16 17:56:11 +0530

    add: prom client register

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Tushar");
+import { DurableObject } from 'cloudflare:workers';
+import { Registry } from 'prom-client';
+
+export default class PromClient<E> extends DurableObject {
+	registry: Registry;
+
+	constructor(state: DurableObjectState, env: E) {
+		super(state, env);
+		this.registry = new Registry();
+	}
+}
